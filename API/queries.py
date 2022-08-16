762a28ae35baf398aacc16b48a377bf66326a684
Reggie Scerbo <rebo@Reggies-MacBook-Pro.local>
2022-08-16 17:38:31 -0400

    created queries file

--- API/queries.py
+++ API/queries.py
@@ -1,6 +1,7 @@
 import mysql.connector
 
-cnx = mysql.connector.connect(user='scott', password='password',
-                              host='127.0.0.1',
-                              database='gym-app')
+cnx = mysql.connector.connect(user='rebo', password='password', host='127.0.0.1', database='gym_app')
+
+
+
 cnx.close()
